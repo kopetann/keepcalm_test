9d3520b9cf92a62b3e1dcf3006b4acd926f4228a
Nikita Komov <komovn2000@gmail.com>
2023-05-23 23:27:39 +0300

    master: third task is done

--- third-back-task.ts
+++ third-back-task.ts
@@ -1,5 +1,20 @@
 export class ThirdBackTask {
-    public static getResult() {
-        return 'Hello World!';
+    public static getResult(sequence: string): string {
+        const matches = [...sequence.matchAll(/(\d){(\d+)}/g)];
+        if (!matches) {
+            return sequence;
+        }
+
+        for (let i = matches.length - 1; i >= 0; i--) {
+            sequence = sequence.replace(matches[i][0], (matches[i][2]).repeat(parseInt(matches[i][1])));
+        }
+
+        if(sequence.includes('{')){
+            sequence = this.getResult(sequence);
+        }
+
+        console.log(sequence);
+
+        return sequence;
     }
 }
