9707657931b808239b4e33774145c613b60f58bd
Nikita Komov <komovn2000@gmail.com>
2023-05-23 21:04:18 +0300

    master: second task is done

--- second-back-task.ts
+++ second-back-task.ts
@@ -1,5 +1,7 @@
 export class SecondBackTask {
-    public static getResult() {
-        return 'Hello World!';
+    public static getResult(base: number, degree: number): number {
+        // In this task we have to calculate the last digit of base raised to the degree
+        // The hack is very-very simple - just convert result to string and slice it!
+        return parseInt((base ** degree).toString().slice(-1));
     }
 }
