4f087d7a740dee1815842e13f25b09acb3387dcc
Nikita Komov <komovn2000@gmail.com>
2023-05-23 23:41:32 +0300

    master: removed unused file

--- main.ts
+++
@@ -1,7 +0,0 @@
-import {FirstBackTask} from './first-back-task';
-import {SecondBackTask} from "./second-back-task";
-import {ThirdBackTask} from "./third-back-task";
-
-const res: number = FirstBackTask.getResult(["start", "connect", "message", "end", "start", "connect", "message", "end", "start", "connect", "message"]);
-const resTwo: number = SecondBackTask.getResult(7.5,3);
-const resThree: string = ThirdBackTask.getResult('4{93{2}}');
